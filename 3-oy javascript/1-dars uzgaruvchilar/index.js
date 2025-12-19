41941a55a204f7fd8feeee328bfde23782513d58
Muhammadjon coding <86595049+MUHAMMADJON0000000000000000000000@users.noreply.github.com>
2025-12-19 19:09:21 +0500

    fist commit

---
+++ 3-oy javascript/1-dars uzgaruvchilar/index.js
@@ -0,0 +1,238 @@
+// input
+// let = uzgaruvchi
+
+// let a = 2 * 3;
+// let P = a * 2;
+// console.log(P);
+
+// let a = 4;
+// let S = a * 2;
+// console.log();
+
+// let = a;
+// let S = a  2;
+// console.log(S);
+
+// 1-misol
+
+// let l = 323;
+// let k = 100;
+// console.log(l / k);
+
+// 2-misol
+
+// let m = 4923;
+// let f = 1000;
+// console.log(m / f);
+
+// 3-misol
+
+// let l = 9824;
+// let f = 1024;
+// console.log(l % f);
+
+// 4-misol
+
+// let a = 918;
+// let b = 132;
+// let d = parseInt(a / b);
+// console.log(d);
+
+// 5-misol
+
+// let k = 957;
+// let m = 71;
+// let g = parseInt(k % m);
+// console.log(g);
+
+// 6-misol
+
+// let l = 23;
+// let k = parseInt(l / 10);
+// let b = l % 10;
+// console.log(k);
+// console.log(b);
+
+// let p = 58;
+// let n = parseInt(p / 10);
+// console.log(n);
+// let y = p % 10;
+// console.log(y);
+
+// parseInt sonni butun qismini chiqaradi
+// % bu sonni qoldig'ini qilsa buladi
+// let a = 71;
+// let j = parseInt(a / 10);
+// let k = a % 10;
+// console.log(j);
+// console.log(k);
+
+// 7-misol
+// let k = 54;
+// let g = parseInt(k / 10);
+// // console.log(g);
+// let f = k % 10;
+// // console.log(f);
+// console.log(f + g);
+
+// 8-misol
+// let h = 32;
+// let f = parseInt(h / 10);
+// let d = h % 10;
+// console.log(d * 10 + f * 1);
+
+// 9-misol
+
+// let d = 351;
+// let g = parseInt(d / 100);
+// console.log(g);
+
+// 10-misol
+// let f = 532;
+// let h = 100;
+// let t = 10;
+// let g = parseInt(f / h); //5
+// let o = f % h; //32
+// let k = parseInt(o / t); //3
+// let p = o % t; //2
+// console.log(p * 10 + k);
+
+// 11 -misol
+// let a = 342;
+// let b = 100;
+// let d = 10;
+// let c = parseInt(a / b); //3
+// let e = a % b; //4,2
+// let s = e % d; //2
+// let g = parseInt(e / d); //4
+
+// console.log(c + s + g);
+
+//12-misol
+// let a = 983;
+// let b = 100;
+// let c = 10;
+// let d = parseInt(a / b);
+// let f = a % b;
+// let g = f % c;
+// let o = parseInt(f / c);
+
+// console.log(d + g + o);
+
+// 13-misol
+// let a = 318;
+// let b = 100;
+// let c = 10;
+// let d = parseInt(a / b); //3
+// let f = a % b;
+// let g = f % c; //8
+// let o = parseInt(f / c); //1
+
+// console.log(o * b + g * c + d);
+
+//14-misol
+// let a = 851;
+// let b = 100;
+// let c = 10;
+// let d = parseInt(a / b); //8
+// let f = a % b;
+// let g = f % c; //1
+// let o = parseInt(f / c); //5
+
+// console.log(g * b + d * c + o);
+
+//15//
+// let a = 423; //243
+// let b = 100;
+// let c = 10;
+// let d = parseInt(a / b); //4
+// let f = a % b;
+// let g = f % c; //3
+// let o = parseInt(f / c); //2
+// console.log(o * b + d * c + g);
+
+//16
+
+// let a = 713; //731
+// let b = 100;
+// let c = 10;
+// let d = parseInt(a / b); //7
+// let f = a % b;
+// let g = f % c; //3
+// let o = parseInt(f / c); //1
+// console.log(d * b + g * c + o);
+
+// //17-misol
+// let a = 5239;
+// let b = a / 100;
+// let d = b % 10;
+// let f = parseInt(d);
+// console.log(f);
+
+// 18-misol
+// let a = 8413;
+// let b = parseInt(a / 100);
+// let d = parseInt(b / 10);
+// console.log(d);
+
+// 19
+let x = 1233;
+console.log();
+
+// parseInt sonni butun qismini chiqaradi
+// % bu sonni qoldig'ini qilsa buladi
+
+12 misollar orasida ketmaketlik va chalkashliklar uchun uzur
+// b = 421;
+// g = parseInt(b / 100); //4
+// c = b % 100; //21
+// a = parseInt(c / 10); //2
+// f = c % 10; //1
+// console.log();
+//13
+// a = 351; //513
+// f = parseInt(a / 100); //3
+// b = a % 100; //51
+// q = parseInt(b / 10); //5
+// g = b % 10; //1
+// console.log(q * 100 + g * 10 + f);
+//14
+// f = 136; //613 change
+// b = parseInt(f / 100); //1
+// q = f % 100; //36
+// a = parseInt(q / 10); //3
+// t = q % 10; //6
+// console.log(t * 100 + a * 10 + b);
+
+//15
+// a = 723; //273
+// b = parseInt(a / 100); //7
+// c = a % 100; //23
+// q = parseInt(c / 10); //3
+// f = c % 10; //3
+
+// console.log(q * 100 + b * 10 + f);
+
+//16
+
+// a = 518; //581
+// c = a % 100; //18
+// v = parseInt(c / 10); //1
+// q = c % 10; //8
+// b = parseInt(a / 100); //5
+// console.log(b * 100 + q * 10 + v);
+
+// 17
+// a = 3181;
+// b = parseInt(a % 1000);
+// q = parseInt(b / 100);
+// console.log(q);
+//18
+// a = 8251;
+// console.log(parseInt(a / 1000));
+
+N = 2931;
+
+// oxirgi misol 19
+a = N / 60;
+console.log(parseInt(a + 1));
